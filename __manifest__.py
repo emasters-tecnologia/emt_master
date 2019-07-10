098c1e1eb13c950e437264a5a8d281cc6eefd514
Eduardo Luiz <bal699@hotmail.com>
2019-07-09 21:29:19 -0300

    #4 - Criado modulo do master.com

---
+++ __manifest__.py
@@ -0,0 +1,21 @@
+# -*- encoding: utf-8 -*-
+{
+    'name': 'Master.com',
+    'summary': 'O melhor aplicativo de gerenciamento de negócios',
+    'version': '12.0.0.0',
+    'category': 'Management',
+    'summary': """
+        O melhor aplicativo de gerenciamento de negócios
+    """,
+    'author': "e-Matsers Tecnologia",
+    'website': '',
+    'license': 'AGPL-3',
+    'depends': [
+    ],
+    'data': [
+    ],
+    'qweb': [
+    ],
+    'installable': True,
+    'application': True,
+}
